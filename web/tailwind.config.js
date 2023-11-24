fdebb97477318185fc706c96aa5434bcc492987f
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2023-11-23 20:06:45 -0800

    fmt: Restructured folders for isolating the web mess by itself

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './ts/**/*.{ts,tsx}',
 	],
   theme: {
     container: {
