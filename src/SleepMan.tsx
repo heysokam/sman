47f793d54da62af7015bb39c69f5188cd04ef587
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2023-11-23 19:50:25 -0800

    new: ShadcnUI | fmt: App changed to SleepMan

--- src/SleepMan.tsx
+++ src/SleepMan.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './SleepMan.css'
+import { Button } from './C/ui/button'
 
-function App() {
+function SleepMan() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='flex items-center'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,20 +17,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <Button onClick={()=> setCount((c)=> c+1) }>Click me. count is {count}</Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default SleepMan
