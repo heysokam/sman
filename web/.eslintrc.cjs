97f6d5fa36ee5b4ec982e4df4d41f0cf4a564808
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2023-11-23 20:26:48 -0800

    fmt: Updated ESLint config and version

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -2,9 +2,10 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'eslint:strict-type-checked',
+    'plugin:@typescript-eslint/strict-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:react-hooks/jsx-runtime',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +15,11 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
 }
